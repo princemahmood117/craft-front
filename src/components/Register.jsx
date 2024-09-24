27468a333d3d55a3d8de217a7009ff75227b91ba
princemahmood117 <princemahmood117@gmail.com>
2024-09-25 05:02:11 +0600

    text color

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -54,7 +54,7 @@ const Register = () => {
            <div className="heromin-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold italic">Register <span className='text-sky-500'>Here!</span> </h1>
+            <h1 className="text-5xl font-bold italic">Register <span className='text-purple-700'>Here!</span> </h1>
          
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
@@ -84,7 +84,7 @@ const Register = () => {
 
 
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Register</button>
+                <button className="btn btn-primary bg-purple-600 border-none">Register</button>
               </div>
 
 
